b06f695986b802a103e876354976620acd857774
Logan <cb58logan@gmail.com>
2022-08-28 18:40:36 +0100

    Added better support for Battle Royale -> To be streamlined in the future

--- Resolver.js
+++ Resolver.js
@@ -1,45 +1,26 @@
 // ==UserScript==
-// @name         Geogussr Resolver
+// @name         geoGuessr Resolver 2.0
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  A Geogussr location resolver.
+// @version      2.0
+// @description  Returns the correct location in Geoguessr
 // @author       0X69ED75
-// @match        https://www.geoguessr.com/game/*
+// @match        https://www.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
 // @grant        none
 // ==/UserScript==
 
-function Grab() {
-    let x = document.getElementsByClassName("game-layout__panorama")[0] // getting the html element containing the props I need.
-    let location = document.getElementsByClassName("game-layout__panorama-canvas")[0].textContent.trim(); // Getting the text supplied in properties, contains small amount of info about the location. Not used now but could be used as a fallback
-    let keys = Object.keys(x) // getting all the keys of the properties of said HTML element.
-    let key = keys.find(key => key.startsWith("__reactFiber$")) // finds the key in the previous array of keys, containing reactFiber, this will contain the state props we are looking for.
-    let foundProps = document.getElementsByClassName("game-layout__panorama")[0][key].child.memoizedProps.children.props // navigating through properties until we find what we want.
+function grab(element) {
+    let x = document.getElementsByClassName(element)[0]  // Getting the HTML element passed as prop's content.
+    let location = x.textContent.trim();// Getting the text supplied in properties, contains small amount of info about the location. Not used now but could be used as a fallback if API fails
+    let keys = Object.keys(x); // getting all the keys of the properties of said HTML element.
+    let key = keys.find(key => key.startsWith("__reactFiber$"))  // finds the key in the previous array of keys, containing reactFiber, this will contain the state props we are looking for.
+    let foundProps = x[key].child.memoizedProps.children.props // going into each property of the html element, until reaching the props needed. Must go into children like this, as I can't yet figure out a viable way to access children directly.
+
     // getting lat and lon from said props.
     let lat = foundProps.lat
     let lon = foundProps.lng
 
     // api call with the lat lon to retrieve data.
-    getAddress(lat,lon).then(data => {
-        console.log(data)
-        alert(`
-    Country: ${data.address.country}
-    County: ${data.address.county}
-    Road: ${data.address.road}
-    State: ${data.address.state}
-    Latitude: ${lat}
-    Longitude: ${lon}
-    `) } );}
-
-function Grab_Online() {
-    let x = document.getElementsByClassName("game_panoramaCanvas__nk_bK")[0] // this value might change on new game. culprit if broke
-    let keys = Object.keys(x);
-    let key = keys.find(key => key.startsWith("__reactFiber$"))
-    let foundProps = x[key]
-
-    let lat = foundProps.memoizedProps.children.props.lat
-    let lon = foundProps.memoizedProps.children.props.lng
-
     getAddress(lat,lon).then(data => {
         console.log(data)
         alert(`
@@ -60,11 +41,26 @@ async function getAddress(lat,lon){
     return data;
 }
 
+/* calculates which game mode the user is in.
+It is worth noting these html elements below aren't the best to use for finding the lon/lat props, but are convenient.
+For BR, it would be better to use "game_panoramaCanvas_nK_Bk" so I have to navigate through less children elements, but, this html element is on the same level as "game-layout__panorama-canvas" so is used so less code is written
+In future updates I should look into the html element with: "data-qa="panorama"" as they share the exact same props regardless of gamemode, meaning this function would be redundant, but for now this will do.
+ */
+function handleGamemode(){
+    if((document.getElementsByClassName("game_panorama__3IFKG")[0]) !== undefined){ // this html element not being undefined suggests the user is in a battle royale game.
+        grab("game_panorama__3IFKG") // passing this elements' name to grab
+    }
+    else if(document.getElementsByClassName("game-layout__panorama-canvas")[0] !== undefined){ // this html not being undefined suggests a user is in a singleplayer game
+        grab("game-layout__panorama") // passing this elements' name to grab
+    }
+    else{alert("Unable to find Location, please try another gamemode.")}
+
+}
+
 
 
 let onKeyDown = (e) => {
-    if(e.keyCode === 86){Grab()}
-    if(e.keyCode === 87){Grab_Online()}
+    if(e.keyCode === 86){handleGamemode()}
 }
 
 
