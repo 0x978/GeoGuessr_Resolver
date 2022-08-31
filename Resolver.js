9831fbfb3ad7879601ce29867908e0f30d60089b
Logan <cb58logan@gmail.com>
2022-08-31 03:57:26 +0100

    Added new feature to instantly win. Rewrote almost all the existing code. Commented everything.

--- Resolver.js
+++ Resolver.js
@@ -1,35 +1,38 @@
 // ==UserScript==
-// @name         geoGuessr Resolver 2.0
+// @name         geoGuessr Resolver 3.0
 // @namespace    http://tampermonkey.net/
-// @version      2.0
-// @description  Returns the correct location in Geoguessr
+// @version      3.0
+// @description  Cheat for Geoguessr, gets max points at the press of a button.
 // @author       0X69ED75
 // @match        https://www.geoguessr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
 // @grant        none
 // ==/UserScript==
 
-function grab(element) {
-    let x = document.getElementsByClassName(element)[0]  // Getting the HTML element passed as prop's content.
-    let location = x.textContent.trim();// Getting the text supplied in properties, contains small amount of info about the location. Not used now but could be used as a fallback if API fails
-    let keys = Object.keys(x); // getting all the keys of the properties of said HTML element.
-    let key = keys.find(key => key.startsWith("__reactFiber$"))  // finds the key in the previous array of keys, containing reactFiber, this will contain the state props we are looking for.
-    let foundProps = x[key].child.memoizedProps.children.props // going into each property of the html element, until reaching the props needed. Must go into children like this, as I can't yet figure out a viable way to access children directly.
+if(localStorage.getItem("firstTimeUsingScript") === null){ // showing a message to user if its their first time using script.
+    localStorage.setItem("firstTimeUsingScript","true")
+    alert(`Thank you for using my script for Geoguessr.
+            Controls:
+            'B': Instantly Guess Correct Answer
+            'V': Show best calculation of current location
+            ----------------------------------------------------------
+            Please Note: Sometimes, the guess correct answer fails, 
+            if this happens just press B again
+            ----------------------------------------------------------`)
+}
 
-    // getting lat and lon from said props.
-    let lat = foundProps.lat
-    let lon = foundProps.lng
-
+function displayLocation() {
+    let coordinates = getCoordinates()
     // api call with the lat lon to retrieve data.
-    getAddress(lat,lon).then(data => {
+    getAddress(coordinates[0],coordinates[1]).then(data => {
         console.log(data)
         alert(`
     Country: ${data.address.country}
     County: ${data.address.county}
     Road: ${data.address.road}
     State: ${data.address.state}
-    Latitude: ${lat}
-    Longitude: ${lon}
+    Latitude: ${coordinates[0]}
+    Longitude: ${coordinates[1]}
     `) } );
 
 }
@@ -41,29 +44,50 @@ async function getAddress(lat,lon){
     return data;
 }
 
-/* calculates which game mode the user is in.
-It is worth noting these html elements below aren't the best to use for finding the lon/lat props, but are convenient.
-For BR, it would be better to use "game_panoramaCanvas_nK_Bk" so I have to navigate through less children elements, but, this html element is on the same level as "game-layout__panorama-canvas" so is used so that less code is written
-In future updates I should look into the html element with: "data-qa="panorama"" as they share the exact same props regardless of gamemode, meaning this function would be redundant, but for now this will do.
- */
-function handleGamemode(){
-    if((document.getElementsByClassName("game_panorama__3IFKG")[0]) !== undefined){ // this html element not being undefined suggests the user is in a battle royale game.
-        grab("game_panorama__3IFKG") // passing this elements' name to grab
-    }
-    else if(document.getElementsByClassName("game-layout__panorama-canvas")[0] !== undefined){ // this html not being undefined suggests a user is in a singleplayer game
-        grab("game-layout__panorama") // passing this elements' name to grab
-    }
-    else if(document.getElementsByClassName("game-panorama_panorama__rdhFg")[0] !== undefined){  // this html not being undefined suggests a user is in a duels game
-        grab("game-panorama_panorama__rdhFg")
-    }
-    else{alert("Unable to find Location, please try another gamemode.")}
+function getCoordinates(){ // Get coordinates through react props exposed by looking at html elements.
+    let x = document.querySelectorAll('[data-qa="panorama"]')[0] // finding the html element where "data-qa = panorama" this should hold the correct coordinates in ALL game modes.
+    let keys = Object.keys(x) // getting the keys of this html element.
+    let key = keys.find(key => key.startsWith("__reactFiber$")) // finding the reactFiber key in this element. This will contain the props we need.
+    let found = x[key].return.memoizedProps // accessing the props containing co-ordinates.
+
+    // accessing both lat and lon
+    let lat = found.lat
+    let lon = found.lng
+
+    return([lat,lon])
 
 }
 
+function placeMarker(){
 
+    let coordinates = getCoordinates()
+
+    let start = document.getElementsByClassName("guess-map__canvas-container")[0] // html element containing needed props.
+    let keys = Object.keys(start) // all keys
+    let key = keys.find(key => key.startsWith("__reactFiber$")) // the React key I need to access props
+    let onMarker = start[key].return.memoizedProps.onMarkerLocationChanged // getting the function which will allow me to place a marker on the map
+
+    onMarker({lat:coordinates[0],lng:coordinates[1]}) // placing the marker on the map at the correct coordinates given by getCoordinates()
+
+    setTimeout(function() { // putting submit guess function in a timeout, to give the react props a second to update. Not doing this causes submission of guess before marker is placed.
+        submitUserGuess()
+    }, 1000);
+}
+
+function submitUserGuess(){
+
+    let element = document.getElementsByClassName("button_button__CnARx button_variantPrimary__xc8Hp")[0] // button element which contains onClick function im looking for.
+    let keys = Object.keys(element).find(key => key.startsWith("__reactFiber$"))
+    let reactKey = element[keys]
+    let submitGuess = reactKey.child.return.memoizedProps.onClick // the oncClick function gained from props. This submits a user's guess.
+
+    submitGuess();
+}
 
 let onKeyDown = (e) => {
-    if(e.keyCode === 86){handleGamemode()}
+    if(e.keyCode === 86){displayLocation()} // key = v
+    if(e.keyCode === 66){placeMarker()} // key = b
+
 }
 
 
