c36622030f2d3245363bcb0c0d11c7d0580282d9
Logan <cb58logan@gmail.com>
2022-10-26 01:06:40 +0100

    Fixed for recent Geoguessr update, as well as updated to a more intuitive control scheme.

--- Resolver.js
+++ Resolver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         geoGuessr Resolver (dev)
 // @namespace    http://tampermonkey.net/
-// @version      4.5
+// @version      6.0
 // @description  Cheat for Geoguessr, gets max points at the press of a button.
 // @author       0X69ED75
 // @match        https://www.geoguessr.com/*
@@ -10,61 +10,30 @@
 // ==/UserScript==
 
 
-alert(`           Thanks for using geoGuessr Resolver by 0x69ED75
+alert(`           Thanks for using geoGuessr Resolver by 0x978.
            ============================================
            => Please use the safer guess Option to avoid bans in competitive. <=
            ============================================
-            Controls:
-            'C': Instantly Place A "Safer" Guess (4500-5000).
-            'B': Instantly Guess Correct Answer
-            'V': Show best calculation of current location
+            Controls (UPDATED!):
+            '1': Place marker on a "safe" guess (4500 - 5000)
+            '2': Place marker on a "perfect" guess (5000)
+            '3': Auto Guess A "safe" Answer (4500 - 5000).
+            '4': Auto Guess "perfect" Answer (5000)
             ----------------------------------------------------------
-            Please Note: Sometimes, the guess correct answer fails,
-            if this happens just press B or C again
+            If auto guess fails, press the key again.
             ----------------------------------------------------------`)
 
 
-function displayLocation() {
-    let coordinates = getCoordinates()
-    // api call with the lat lon to retrieve data.
-    getAddress(coordinates[0],coordinates[1]).then(data => {
-        console.log(data)
-        alert(`
-    Country: ${data.address.country}
-    County: ${data.address.county}
-    Road: ${data.address.road}
-    State: ${data.address.state}
-    Latitude: ${coordinates[0]}
-    Longitude: ${coordinates[1]}
-    `) } );
-
-}
-
-// async function returns JSON from API call.
 async function getAddress(lat,lon){
     let response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json`)
     let data = await response.json()
     return data;
 }
 
-function getCoordinates(){ // Get coordinates through react props exposed by looking at html elements.
-    let x = document.querySelectorAll('[data-qa="panorama"]')[0] // finding the html element where "data-qa = panorama" this should hold the correct coordinates in ALL game modes.
-    let keys = Object.keys(x) // getting the keys of this html element.
-    let key = keys.find(key => key.startsWith("__reactFiber$")) // finding the reactFiber key in this element. This will contain the props we need.
-    let found = x[key].return.memoizedProps // accessing the props containing co-ordinates.
-
-    // accessing both lat and lon
-    let lat = found.lat
-    let lon = found.lng
-
-    return([lat,lon])
-
-}
-
-function placeMarker(safeMode){
+function placeMarker(safeMode,instantGuess){
 
     if(document.getElementsByClassName("guess-map__canvas-container")[0] === undefined){ // if this is not defined, the user must be in a streaks game, streaks mode uses a different map and therefore is calculated in a different function
-        placeMarkerStreaksMode()
+        placeMarkerStreaksMode(instantGuess)
         return;
     }
 
@@ -81,12 +50,14 @@ function placeMarker(safeMode){
 
     onMarker({lat:coordinates[0],lng:coordinates[1]}) // placing the marker on the map at the correct coordinates given by getCoordinates()
 
-    setTimeout(function() { // putting submit guess function in a timeout, to give the react props a second to update. Not doing this causes submission of guess before marker is placed.
-        submitUserGuess()
-    }, 1000);
+    if(instantGuess){
+        setTimeout(function() { // putting submit guess function in a timeout, to give the react props a second to update. Not doing this causes submission of guess before marker is placed.
+            submitUserGuess()
+        }, 1000);
+    }
 }
 
-function placeMarkerStreaksMode(){
+function placeMarkerStreaksMode(instantGuess){
     let coordinates = getCoordinates()
     let countryCode = ""
 
@@ -105,9 +76,34 @@ function placeMarkerStreaksMode(){
         placeMarkerFunction(countryCode) // passing the country code into the onRegionSelected method.
     })
 
-    setTimeout(function() { // putting submit guess function in a timeout, to give the react props a second to update. Not doing this causes submission of guess before marker is placed.
-        submitUserGuess()
-    }, 1000);
+    if(instantGuess){
+        setTimeout(function() { // putting submit guess function in a timeout, to give the react props a second to update. Not doing this causes submission of guess before marker is placed.
+            submitUserGuess()
+        }, 1000);
+    }
+}
+
+// detects game mode and return appropriate coordinates.
+function getCoordinates(){
+    let element;
+    let keys;
+    let key;
+    let found;
+    //competitive
+    if(document.getElementsByClassName("game-panorama_panorama__rdhFg").length > 0){
+        element = document.getElementsByClassName("game-panorama_panorama__rdhFg")[0] // element for competitive
+        keys = Object.keys(element) // all keys
+        key = keys.find(key => key.startsWith("__reactFiber$")) // needed key
+        found = element[key].return.memoizedProps.panorama // location of coords
+    }
+    else{
+        element = document.querySelectorAll('[data-qa="panorama"]')[0] // finding the html element where "data-qa = panorama" this should hold the correct coordinates in ALL game modes.
+        keys = Object.keys(element) // getting the keys of this html element.
+        key = keys.find(key => key.startsWith("__reactFiber$")) // finding the reactFiber key in this element. This will contain the props we need.
+        found = element[key].return.memoizedProps // accessing the props containing co-ordinates.
+    }
+    return([found.lat,found.lng])
+
 }
 
 function submitUserGuess(){
@@ -120,11 +116,12 @@ function submitUserGuess(){
     submitGuess();
 }
 
-let onKeyDown = (e) => {
-    if(e.keyCode === 86){displayLocation()} // key = v
-    if(e.keyCode === 66){placeMarker(false)} // key = b
-    if(e.keyCode === 67){placeMarker(true)} // key = c
 
+let onKeyDown = (e) => {
+    if(e.keyCode === 49){placeMarker(true,false)}
+    if(e.keyCode === 50){placeMarker(false,false)}
+    if(e.keyCode === 51){placeMarker(true,true)}
+    if(e.keyCode === 67){placeMarker(false,true)}
 }
 
 
